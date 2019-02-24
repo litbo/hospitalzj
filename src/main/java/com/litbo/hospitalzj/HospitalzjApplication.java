20bc6442b1e885a3c21290fdb3dda572415e3b17
litbo <973046515@qq.com>
2019-02-24 20:32:21 +0800

    test

---
+++ src/main/java/com/litbo/hospitalzj/HospitalzjApplication.java
@@ -0,0 +1,13 @@
+package com.litbo.hospitalzj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalzjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalzjApplication.class, args);
+    }
+
+}
